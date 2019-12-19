8a1908a8397e23fd52301949bec4637dbfdf7c31
Stéphane Dailly <sdailly@altima-agency.com>
2019-12-19 11:29:01 +0100

    build bundle demo

--- demo/demo.bundle.js
+++ demo/demo.bundle.js
@@ -42,7 +42,7 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
 
@@ -59,100 +59,172 @@
 	  });
 	}, false);
 
-/***/ },
+/***/ }),
 /* 1 */
-/***/ function(module, exports) {
+/***/ (function(module, exports) {
 
-	"use strict";
+	'use strict';
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var Countdown = function () {
-	  function Countdown(options) {
-	    var _this = this;
+	  function Countdown() {
+	    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 	    _classCallCheck(this, Countdown);
 
-	    if (!options.targetTime || !options.id) {
-	      console.error("Countdown : Date is not defined");
-	    }
+	    this.options = options;
+	    this.targetTime = new Date(this.options.targetTime).getTime();
+	    this.afterEnd = this.options.afterEnd;
+	    this.hideDayAtZero = this.options.hideDayAtZero || false;
+	    this.noDay = this.options.noDay || false;
 
-	    this.el = document.getElementById(options.id);
-	    this.now = new Date().getTime();
-	    this.targetTime = new Date(options.targetTime).getTime();
-	    this.afterEnd = options.afterEnd;
-	    if (options.separator === false) {
-	      this.separator = "";
-	    } else {
-	      this.separator = options.separator && typeof options.separator == "string" ? options.separator : ":";
-	    }
-	    this.hideDayAtZeroOpts = options.hideDayAtZero || false;
-	    this.noDay = options.noDay || false;
-
-	    if (this.now > this.targetTime) {
-	      console.warn("Countdown : Date defined > Date now");
-	      return;
-	    }
-
-	    this.refresh();
-	    this.interval = setInterval(function () {
-	      _this.refresh();
-	    }, 1000);
+	    this.checkExceptions();
+	    this.defineSeparatorTpl();
+	    this.run();
 	  }
 
 	  _createClass(Countdown, [{
-	    key: "refresh",
+	    key: 'checkExceptions',
+	    value: function checkExceptions() {
+	      this.optionsAreCorrectlyDefined();
+	      this.checkDateIsOutOfTargetDate();
+	    }
+	  }, {
+	    key: 'optionsAreCorrectlyDefined',
+	    value: function optionsAreCorrectlyDefined() {
+	      var targetTimeOptionExist = this.options.hasOwnProperty('targetTime');
+	      var idOptionExist = this.options.hasOwnProperty('id');
+
+	      if (!targetTimeOptionExist || !idOptionExist) {
+	        throw new Error("Countdown : Date is not defined");
+	      }
+	    }
+	  }, {
+	    key: 'checkDateIsOutOfTargetDate',
+	    value: function checkDateIsOutOfTargetDate() {
+	      if (this.now > this.targetTime) {
+	        throw new Error("Countdown : Date defined > Date now");
+	      }
+	    }
+	  }, {
+	    key: 'defineSeparatorTpl',
+	    value: function defineSeparatorTpl() {
+	      var separatorOptionSetToFalse = this.options.separator === false;
+	      if (separatorOptionSetToFalse) {
+	        this.separator = "";
+	      } else {
+	        this.separator = this.options.separator || ':';
+	      }
+	    }
+	  }, {
+	    key: 'run',
+	    value: function run() {
+	      var _this = this;
+
+	      this.refresh();
+	      this.intervalRender = setInterval(function () {
+	        _this.refresh();
+	      }, 1000);
+	    }
+	  }, {
+	    key: 'refresh',
 	    value: function refresh() {
 	      this.now = new Date().getTime();
-
+	      this.checkTimeOutOrNot();
+	    }
+	  }, {
+	    key: 'checkTimeOutOrNot',
+	    value: function checkTimeOutOrNot() {
 	      if (this.now > this.targetTime) {
-	        clearInterval(this.interval);
-	        if (this.afterEnd) this.afterEnd();
-	        return;
-	      }
-
-	      this.defineTimer();
-	      this.render();
-	    }
-	  }, {
-	    key: "hideDayAtZero",
-	    value: function hideDayAtZero() {
-	      if (this.hideDayAtZeroOpts && this.timer.day == 0) {
-	        return true;
+	        this.clearAndRunCallback();
+	      } else {
+	        this.defineTimer();
+	        this.render();
 	      }
 	    }
 	  }, {
-	    key: "defineTimer",
+	    key: 'clearAndRunCallback',
+	    value: function clearAndRunCallback() {
+	      clearInterval(this.intervalRender);
+	      if (this.afterEnd) this.afterEnd();
+	    }
+	  }, {
+	    key: 'hideDay',
+	    value: function hideDay() {
+	      return !!(this.hideDayAtZero && (this.timer.day === 0 || this.timer.day === '00'));
+	    }
+	  }, {
+	    key: 'defineTimer',
 	    value: function defineTimer() {
 	      this.timer = {};
-	      var tmp = this.targetTime - this.now;
-	      tmp = Math.floor(tmp / 1000);
-	      this.timer.sec = tmp % 60;
-	      this.timer.sec = this.timer.sec < 10 ? "0" + this.timer.sec : this.timer.sec;
+	      var diffTime = this.targetTime - this.now;
 
-	      tmp = Math.floor((tmp - this.timer.sec) / 60);
-	      this.timer.min = tmp % 60;
-	      this.timer.min = this.timer.min < 10 ? "0" + this.timer.min : this.timer.min;
+	      diffTime = Math.floor(diffTime / 1000);
+	      this.setTimerToSec(diffTime);
 
-	      tmp = Math.floor((tmp - this.timer.min) / 60);
-	      this.timer.hour = this.noDay ? tmp : tmp % 24;
-	      this.timer.hour = this.timer.hour < 10 ? "0" + this.timer.hour : this.timer.hour;
+	      diffTime = Math.floor((diffTime - this.timer.sec) / 60);
+	      this.setTimerToMin(diffTime);
+
+	      diffTime = Math.floor((diffTime - this.timer.min) / 60);
+	      this.setTimeToHours(diffTime);
 
 	      if (this.noDay) return;
-	      tmp = Math.floor((tmp - this.timer.hour) / 24);
-	      this.timer.day = tmp;
-	      if (this.hideDayAtZero()) return;
-	      this.timer.day = this.timer.day < 10 ? "0" + this.timer.day : this.timer.day;
+	      var remainingDay = Math.floor((diffTime - this.timer.hour) / 24);
+	      this.timer.day = remainingDay;
+	      this.addPadStartZero(remainingDay, "day");
 	    }
 	  }, {
-	    key: "render",
+	    key: 'addPadStartZero',
+	    value: function addPadStartZero(time, timer) {
+	      if (time < 10) {
+	        this.timer[timer] = time.toString().padStart(2, '0');
+	      }
+	    }
+	  }, {
+	    key: 'setTimeToHours',
+	    value: function setTimeToHours(tmp) {
+	      var remainingHours = this.noDay ? tmp : tmp % 24;
+	      this.timer.hour = remainingHours;
+	      this.addPadStartZero(remainingHours, "hour");
+	    }
+	  }, {
+	    key: 'setTimerToMin',
+	    value: function setTimerToMin(tmp) {
+	      var remainingMin = tmp % 60;
+	      this.timer.min = remainingMin;
+	      this.addPadStartZero(remainingMin, "min");
+	    }
+	  }, {
+	    key: 'setTimerToSec',
+	    value: function setTimerToSec(tmp) {
+	      var remainingSec = tmp % 60;
+	      this.timer.sec = remainingSec;
+	      this.addPadStartZero(remainingSec, "sec");
+	    }
+	  }, {
+	    key: 'render',
 	    value: function render() {
-	      var tplDay = "<span class=\"ds-Countdown-item ds-Countdown-item--day\">" + this.timer.day + "</span>" + this.separator;
-	      var tpl = "<span class=\"ds-Countdown-item ds-Countdown-item--hour\">" + this.timer.hour + "</span>" + this.separator + "<span class=\"ds-Countdown-item ds-Countdown-item--min\">" + this.timer.min + "</span>" + this.separator + "<span class=\"ds-Countdown-item ds-Countdown-item--sec\">" + this.timer.sec + "</span>";
+	      var _createTemplate = this.createTemplate(),
+	          tplDay = _createTemplate.tplDay,
+	          tpl = _createTemplate.tpl;
 
-	      this.el.innerHTML = this.hideDayAtZero() || this.noDay ? tpl : tplDay + tpl;
+	      this.el = document.getElementById(this.options.id);
+	      this.el.innerHTML = this.hideDay() || this.noDay ? tpl : tplDay + tpl;
+	    }
+	  }, {
+	    key: 'createItem',
+	    value: function createItem(timer, modifier) {
+	      return '<span class="ds-Countdown-item ds-Countdown-item--' + modifier + '">' + timer + '</span>';
+	    }
+	  }, {
+	    key: 'createTemplate',
+	    value: function createTemplate() {
+	      var tplDay = this.createItem(this.timer.day, 'day') + this.separator;
+	      var tpl = this.createItem(this.timer.hour, 'hour') + this.separator + this.createItem(this.timer.min, 'min') + this.separator + this.createItem(this.timer.sec, 'sec');
+	      return { tplDay: tplDay, tpl: tpl };
 	    }
 	  }]);
 
@@ -161,5 +233,5 @@
 
 	module.exports = Countdown;
 
-/***/ }
+/***/ })
 /******/ ]);
